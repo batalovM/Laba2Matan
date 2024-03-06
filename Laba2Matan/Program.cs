8abc7538dff9cc970df5d26eb364bc9b7145286c
batalovM <b.maksim1708@gmail.com>
2024-03-06 13:09:25 +0700

    upt

--- Laba2Matan/Program.cs
+++ Laba2Matan/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var eps = 0.001f; // Параметр точности
+        const float eps = 0.001f; // Параметр точности
         float[,] a1 = { 
             {5f, 3f, 1f},
             {3f,1.79999f, 7f},
@@ -16,6 +16,18 @@ class Program
             12f,
             13.59998f, 
             18f };
+        var result1 = new GaussMethod(a1, b1).GaussWithoutMainElement();
+        Console.WriteLine("Решение методом Гаусса без выбора главного элемента:");
+        foreach (var x in result1)
+        {
+            Console.WriteLine(x);
+        }
+        var result2 = new GaussMethod(a1, b1).GaussWithMainElement();
+        Console.WriteLine("Решение методом Гаусса с выбором главного элемента:");
+        foreach (var x in result2)
+        {
+            Console.WriteLine(x);
+        }
         
         float[,] a2 = { 
             {2f, 15f, -1f},
@@ -29,34 +41,34 @@ class Program
             29f,
             25f, 
             53f };
-        var result1 = new GaussMethod(a1, b1).GaussWithoutMainElement();
-        
-        
-        Console.WriteLine("Решение методом Гаусса без выбора главного элемента:");
-        foreach (var x in result1)
-        {
-            Console.WriteLine(x);
-        }
-        var result2 = new GaussMethod(a1, b1).GaussWithMainElement();
-        Console.WriteLine("Решение методом Гаусса с выбором главного элемента:");
-        foreach (var x in result2)
-        {
-            Console.WriteLine(x);
-        }
         var result3 = new SimpleIterations(a3, b2, eps).SimpleSolution();
         Console.WriteLine("Решение методом простых итераций:");
         foreach (var x in result3)
         {
             Console.WriteLine(x);
         }
-
-        // var result4 = new Progon(A1, b1).Solve();
-        // Console.WriteLine("Решение методом прогонки:");
-        // foreach (var x in result4)
-        // {
-        //     Console.WriteLine(x);
-        // }
-
+        
+        float[,] matrixA =
+        {
+            { 2, -1, 0, 0, 0, -25 },
+            { -3, 8, -1, 0, 0, 72 },
+            { 0, -5, 12, 2, 0, -69 },
+            { 0, 0, -6, 18, -4, -156 },
+            { 0, 0, 0, -5, 10, 20 }
+        };
+        float[] matrixB = { 
+            -25f,
+            72f, 
+            -69f,
+            -156f,
+            20f
+        };
+        var result4 = new Progon(matrixA, matrixB).TridiagonalSolver();
+        Console.WriteLine("Решение методом прогонки:");
+        foreach (var x in result4)
+        {
+            Console.WriteLine(x);
+        }
     }
     
 }
